e34fe08694b1c60ca68d99873981742aa68e6ecf
Elliott Chen <cyfyifanchen@gmail.com>
2024-03-20 14:19:06 +0800

    Adding footer

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,9 +1,25 @@
-const Footer = () => {
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button
+          size="lg"
+          variant="ghost"
+          className="w-full"
+        >
+          <Image
+            src="/hr.svg"
+            alt="Croation"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Croation
+        </Button>
+      </div>
     </footer>
   )
 }
-
-export default Footer
