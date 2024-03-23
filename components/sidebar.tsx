6c22b75351db49c5568da73d1422355065556d28
贝吉塔大王 <cyfyifanchen@gmail.com>
2024-03-23 10:38:25 +0800

    Adding all items in the sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,6 +1,10 @@
-import { cn } from '@/lib/utils'
-import Image from 'next/image'
 import Link from 'next/link'
+import Image from 'next/image'
+import { ClerkLoading, ClerkLoaded, UserButton } from '@clerk/nextjs'
+import { Loader } from 'lucide-react'
+
+import { cn } from '@/lib/utils'
+
 import { SidebarItem } from './sidebar-item'
 
 type Props = {
@@ -11,7 +15,7 @@ export const Sidebar = ({ className }: Props) => {
   return (
     <div
       className={cn(
-        'flex  h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col',
+        'flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col',
         className
       )}
     >
@@ -24,16 +28,39 @@ export const Sidebar = ({ className }: Props) => {
             alt="Mascot"
           />
           <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
-            Duolingooo
+            Lingo
           </h1>
         </div>
       </Link>
-      <div className="flex flex-col gpy-y-2 flex-1">
+      <div className="flex flex-col gap-y-2 flex-1">
         <SidebarItem
           label="Learn"
-          iconSrc="/learn.svg"
           href="/learn"
+          iconSrc="/learn.svg"
         />
+        <SidebarItem
+          label="Leaderboard"
+          href="/leaderboard"
+          iconSrc="/leaderboard.svg"
+        />
+        <SidebarItem
+          label="quests"
+          href="/quests"
+          iconSrc="/quests.svg"
+        />
+        <SidebarItem
+          label="shop"
+          href="/shop"
+          iconSrc="/shop.svg"
+        />
+      </div>
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton afterSignOutUrl="/" />
+        </ClerkLoaded>
       </div>
     </div>
   )
