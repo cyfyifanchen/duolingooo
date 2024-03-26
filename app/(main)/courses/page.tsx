1979d978c15e9586e1a2806691a1bbe6a9c837c6
贝吉塔大王 <cyfyifanchen@gmail.com>
2024-03-26 21:36:59 +0800

    Fixing the typo

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,4 +1,5 @@
 import { getCourses } from '@/db/queries'
+import { List } from 'lucide-react'
 
 const CoursesPage = async () => {
   const data = await getCourses()
@@ -6,7 +7,10 @@ const CoursesPage = async () => {
   return (
     <div className="h-full max-w-[912px] px-3 mx-auto">
       <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
-      {JSON.stringify(data)}
+      <List
+      // courses={courses}
+      // activeCourseId={1}
+      />
     </div>
   )
 }
--- db/queries.ts
+++ db/queries.ts
@@ -2,6 +2,6 @@ import { cache } from 'react'
 import db from './drizzle'
 
 export const getCourses = cache(async () => {
-  const data = await db.query.course.findMany()
+  const data = await db.query.courses.findMany()
   return data
 })
