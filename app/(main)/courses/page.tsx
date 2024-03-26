d3c2e51585c905bed050945f884908fb74c42e44
贝吉塔大王 <cyfyifanchen@gmail.com>
2024-03-26 21:42:03 +0800

    Adding courses list and courses page

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,5 +1,5 @@
 import { getCourses } from '@/db/queries'
-import { List } from 'lucide-react'
+import { List } from './list'
 
 const CoursesPage = async () => {
   const data = await getCourses()
@@ -8,8 +8,8 @@ const CoursesPage = async () => {
     <div className="h-full max-w-[912px] px-3 mx-auto">
       <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
       <List
-      // courses={courses}
-      // activeCourseId={1}
+        courses={courses}
+        activeCourseId={1}
       />
     </div>
   )
