ee60622684319e8956c64ba44f4018302949b090
贝吉塔大王 <cyfyifanchen@gmail.com>
2024-03-27 07:33:43 +0800

    Adding card component

---
+++ app/(main)/courses/card.tsx
@@ -0,0 +1,12 @@
+type Props = {
+  title: string
+  id: number
+  imageSrc: string
+  onClick: (id: number) => void
+  disabled?: boolean
+  active?: boolean
+}
+
+export const Card = ({}: Props) => {
+  return <div className="">this is card</div>
+}
--- app/(main)/courses/list.tsx
+++ app/(main)/courses/list.tsx
@@ -1,12 +1,27 @@
 'use client'
 
 import { courses } from '@/db/schema'
+import { Card } from './card'
 
 type Props = {
   courses: (typeof courses.$inferSelect)[]
   activeCourseId: number
 }
 
-export const List = ({}: Props) => {
-  return <div className="pt-6 grid grid-cols-2">List</div>
+export const List = ({ courses, activeCourseId }: Props) => {
+  return (
+    <div className="pt-6 grid grid-cols-2 lg:grid-cols-[repeat(auto-fill,minmax(210,1fr))]">
+      {courses.map((course) => (
+        <Card
+          key={course.id}
+          id={course.id}
+          title={course.title}
+          imageSrc={course.imageSrc}
+          onClick={() => {}}
+          disabled={false}
+          active={course.id === activeCourseId}
+        />
+      ))}
+    </div>
+  )
 }
