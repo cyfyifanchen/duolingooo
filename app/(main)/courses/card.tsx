4a124876c4a7d9786d04a82a1aacc1037197bb0d
贝吉塔大王 <cyfyifanchen@gmail.com>
2024-03-27 08:08:00 +0800

    DB has an issue now

--- app/(main)/courses/card.tsx
+++ app/(main)/courses/card.tsx
@@ -1,3 +1,5 @@
+import { cn } from '@/lib/utils'
+
 type Props = {
   title: string
   id: number
@@ -7,6 +9,21 @@ type Props = {
   active?: boolean
 }
 
-export const Card = ({}: Props) => {
-  return <div className="">this is card</div>
+export const Card = ({
+  title,
+  id,
+  imageSrc,
+  onClick,
+  disabled,
+  active,
+}: Props) => {
+  return (
+    <div
+      className={cn(
+        'h-full border-2 rounded-xl border-b-4 hover:bg-black/5 cursor-pointer active:border-b-2 flex flex-col items-center justify-between p-3 pb-6 min-h-[217px] min-w-[200px]',
+        disabled && 'pointer-events-none opacity-50'
+      )}
+      onClick={() => onClick(id)}
+    ></div>
+  )
 }
