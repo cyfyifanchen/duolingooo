4472a8084a73f9d5671b0bd74219b28f38a99db4
贝吉塔大王 <cyfyifanchen@gmail.com>
2024-03-30 14:37:06 +0800

    Adding userpgoress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,8 +2,22 @@ import { FeedWrapper } from '@/components/feed-wrapper'
 import { StickyWrapper } from '@/components/sticky-wrapper'
 import { Header } from './header'
 import { UserProgress } from '@/components/user-progress'
+import { getCourses, getUserProgress } from '@/db/queries'
+import { redirect } from 'next/navigation'
 
-const LearnPage = () => {
+const LearnPage = async () => {
+  const coursesData = getCourses()
+  const userProgressData = getUserProgress()
+
+  const [courses, userProgress] = await Promise.all([
+    coursesData,
+    userProgressData,
+  ])
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect('/courses')
+  }
+
   return (
     <div className="flex gap-[48px] px-6">
       <FeedWrapper>
