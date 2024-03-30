c05b4cac7940e03acebb85f5b47a3e6bc9e35096
贝吉塔大王 <cyfyifanchen@gmail.com>
2024-03-30 09:42:38 +0800

    Update page.tsx

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -8,7 +8,7 @@ const CoursesPage = async () => {
     <div className="h-full max-w-[912px] px-3 mx-auto">
       <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
       <List
-        courses={courses}
+        courses={course}
         activeCourseId={1}
       />
     </div>
