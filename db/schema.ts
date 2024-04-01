9de05da435b3638fe07159ae483b4e9e62f9caae
Elliott Chen <cyfyifanchen@gmail.com>
2024-04-01 11:45:05 +0800

    Update schema.ts

--- db/schema.ts
+++ db/schema.ts
@@ -78,7 +78,7 @@ export const challengesRelations = relations(challenges, ({ one, many }) => ({
   challengeProgress: many(challengeProgress),
 }))
 
-export const challengeOptions = pgTable('challengeOptions', {
+export const challengeOptions = pgTable('challenge_options', {
   id: serial('id').primaryKey(),
   challengeId: integer('challenge_id')
     .references(() => challenges.id, { onDelete: 'cascade' })
@@ -99,7 +99,7 @@ export const challengeOptionsRelations = relations(
   })
 )
 
-export const challengeProgress = pgTable('challengeProgress', {
+export const challengeProgress = pgTable('challenge_progress', {
   id: serial('id').primaryKey(),
   userId: text('user_id').notNull(),
   challengeId: integer('challenge_id')
