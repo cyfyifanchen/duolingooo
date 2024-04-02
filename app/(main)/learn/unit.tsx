9391837b79ab531b70d15b33798d85f9aa01658d
Elliott Chen <cyfyifanchen@gmail.com>
2024-04-02 14:23:20 +0800

    Adding lesson-button

--- app/(main)/learn/unit.tsx
+++ app/(main)/learn/unit.tsx
@@ -1,5 +1,6 @@
 import { lessons, units } from '@/db/schema'
 import { UnitBanner } from './unit-banner'
+import { LessonButton } from './lesson-button'
 
 type Props = {
   id: number
@@ -32,6 +33,24 @@ export const Unit = ({
         title={title}
         description={description}
       />
+      <div className="flex items-center flex-col relative">
+        {lessons.map((lesson, index) => {
+          const isCurrent = lesson.id === activeLesson?.id
+          const isLocked = !lesson.completed && !isCurrent
+
+          return (
+            <LessonButton
+              key={lesson.id}
+              id={lesson.id}
+              index={index}
+              totalCount={lessons.length - 1}
+              current={isCurrent}
+              locked={isLocked}
+              percentage={activeLessonPercentage}
+            />
+          )
+        })}
+      </div>
     </>
   )
 }
