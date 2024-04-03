d28bf47ca8a4e301f8c9f6f2e675315a28a69f7d
Elliott Chen <cyfyifanchen@gmail.com>
2024-04-03 15:33:24 +0800

    Adding header and layout for lesson

--- app/lesson/layout.tsx
+++ app/lesson/layout.tsx
@@ -5,7 +5,7 @@ type Props = {
 const LessonLayout = ({ children }: Props) => {
   return (
     <div className="flex flex-col h-full">
-      <div className="flex flex-col h-full w-full">Header</div>
+      <div className="flex flex-col h-full w-full">{children}</div>
     </div>
   )
 }
