31bd7d5af5de2448785ff4bae7a3f10adb6da2e4
贝吉塔大王 <cyfyifanchen@gmail.com>
2024-04-03 23:05:40 +0800

    Update quiz.tsx

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -32,6 +32,11 @@ export const Quiz = ({
         percentage={percentage}
         hasActiveSubscription={!!userSubscription?.isActive}
       />
+      <div className="flex-1">
+        <div className="h-full flex items-center justify-center">
+          <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12"></div>
+        </div>
+      </div>
     </>
   )
 }
