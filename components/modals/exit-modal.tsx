2c319f1c1f40cd66b7b499e64b0cbda007580389
Elliott Chen <cyfyifanchen@gmail.com>
2024-04-03 18:24:03 +0800

    Create exit-modal.tsx

---
+++ components/modals/exit-modal.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+
+export const ExitModal = () => {
+  const router = useRouter()
+  const [isClient, setIsClient] = useState(false)
+}
