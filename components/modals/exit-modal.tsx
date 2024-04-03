e9184391937e2976abe08510533f9f2957515d15
贝吉塔大王 <cyfyifanchen@gmail.com>
2024-04-03 22:16:07 +0800

    Adding exit-modal

--- components/modals/exit-modal.tsx
+++ components/modals/exit-modal.tsx
@@ -1,9 +1,15 @@
 'use client'
 
+import { useExitModal } from '@/store/use-exit-modal'
 import { useRouter } from 'next/router'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export const ExitModal = () => {
   const router = useRouter()
   const [isClient, setIsClient] = useState(false)
+  const { isOpen, close } = useExitModal()
+
+  useEffect(() => setIsClient(true), [])
+
+  if (!isClient) return null
 }
