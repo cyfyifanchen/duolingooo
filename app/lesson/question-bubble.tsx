38f13e3278a83b595d9434f61a8d14cba13710bf
贝吉塔大王 <cyfyifanchen@gmail.com>
2024-04-04 09:48:16 +0800

    Adding question-bubble

---
+++ app/lesson/question-bubble.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+type Props = {
+  question: string
+}
+
+export const QuestionBubble = ({ question }: Props) => {
+  return <div className="">This is the bubble</div>
+}
