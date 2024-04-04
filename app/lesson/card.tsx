49bf70ea977c7df2c3baad395df4397c8b92dcc6
贝吉塔大王 <cyfyifanchen@gmail.com>
2024-04-04 12:40:22 +0800

    Adding images

--- app/lesson/card.tsx
+++ app/lesson/card.tsx
@@ -1,5 +1,6 @@
 import { challenges } from '@/db/schema'
 import { cn } from '@/lib/utils'
+import Image from 'next/image'
 
 type Props = {
   id: number
@@ -29,11 +30,28 @@ export const Card = ({
   return (
     <div
       className={cn(
-        'h-full border-2 rounded-xl border-b-4 hover:bg-black/5 p-4 lg:p-6 cursor-pointer active:border-b-2'
+        'h-full border-2 rounded-xl border-b-4 hover:bg-black/5 p-4 lg:p-6 cursor-pointer active:border-b-2',
+        selected && 'border-sky-300 bg-sky-100 hover:bg-sky-100',
+        selected &&
+          status === 'correct' &&
+          'border-green-300 bg-green-100 hover:bg-green-100',
+        selected &&
+          status === 'wrong' &&
+          'border-rose-300 bg-rose-300 hover:bg-rose-100',
+        disabled && 'pointer-events-none hover:bg-white',
+        type === 'ASSIST' && 'lg:p-3 w-full'
       )}
       onClick={() => {}}
     >
-      Card
+      {imageSrc && (
+        <div className="relative aspect-square mb-4 max-h-[80px] lg:max-h-[150px] w-full">
+          <Image
+            src={imageSrc}
+            fill
+            alt={text}
+          />
+        </div>
+      )}
     </div>
   )
 }
