44840cef50f58db918168daa0d0a9cfd92013907
贝吉塔大王 <cyfyifanchen@gmail.com>
2024-04-05 06:55:16 +0800

    Adding quiz onNext

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -41,10 +41,39 @@ export const Quiz = ({
   const challenge = challenges[activeIndex]
   const options = challenge?.challengeOptions ?? []
 
+  const onNext = () => {
+    setActiveIndex((current) => current + 1)
+  }
+
   const onSelect = (id: number) => {
     if (status !== 'none') return
 
     setSelectedOption(id)
+  }
+
+  const onContinue = () => {
+    if (!selectedOption) return
+
+    if (status === 'correct') {
+      onNext()
+      setStatus('none')
+      setSelectedOption(undefined)
+      return
+    }
+
+    if (status === 'wrong') {
+      setStatus('none')
+      setSelectedOption(undefined)
+      return
+    }
+
+    const correctOption = options.find((option) => option.correct)
+
+    if (correctOption && correctOption.id === selectedOption) {
+      console.log('Correct option!')
+    } else {
+      console.error('Incorrect option.')
+    }
   }
   const title =
     challenge.type === 'ASSIST'
@@ -82,7 +111,7 @@ export const Quiz = ({
       <Footer
         disabled={!selectedOption}
         status={status}
-        onCheck={() => {}}
+        onCheck={onContinue}
       />
     </>
   )
