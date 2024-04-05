1305f192386167bd9f78fa5ba0d4bca2dd397e6a
贝吉塔大王 <cyfyifanchen@gmail.com>
2024-04-05 08:34:51 +0800

    Adding challenge-progress in actions

---
+++ actions/challenge-progress.tsx
@@ -0,0 +1,33 @@
+'use server'
+
+import db from '@/db/drizzle'
+import { getUserProgress } from '@/db/queries'
+import { challengeProgress, challenges } from '@/db/schema'
+import { auth } from '@clerk/nextjs'
+import { and, eq } from 'drizzle-orm'
+
+export const upsertChallengeProgress = async (challengeId: number) => {
+  const { userId } = await auth()
+  if (!userId) throw new Error('Unauthorized')
+
+  const currentUserProgress = await getUserProgress()
+
+  if (!currentUserProgress) throw new Error('User progress not found.')
+
+  const challenge = await db.query.challenges.findFirst({
+    where: eq(challenges.id, challengeId),
+  })
+
+  if (!challenge) throw new Error('Challenge not found.')
+
+  const lessonId = challenge.lessonId
+
+  const existingChallengeProgress = await db.query.challengeProgress.findFirst({
+    where: and(
+      eq(challengeProgress.userId, userId),
+      eq(challengeProgress.challengeId, challengeId)
+    ),
+  })
+
+  const isPractice = !!existingChallengeProgress
+}
