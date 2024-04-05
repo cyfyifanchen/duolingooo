e53b74bb4783b35b78ba0eeadcf033b307a571bf
贝吉塔大王 <cyfyifanchen@gmail.com>
2024-04-05 09:34:44 +0800

    Update challenge progress

--- actions/challenge-progress.tsx
+++ actions/challenge-progress.tsx
@@ -3,8 +3,9 @@
 import db from '@/db/drizzle'
 import { getUserProgress } from '@/db/queries'
 import { challengeProgress, challenges, userProgress } from '@/db/schema'
-import { auth } from '@clerk/nextjs'
+import { auth, currentUser } from '@clerk/nextjs'
 import { and, eq } from 'drizzle-orm'
+import { revalidatePath } from 'next/cache'
 
 export const upsertChallengeProgress = async (challengeId: number) => {
   const { userId } = await auth()
@@ -45,13 +46,39 @@ export const upsertChallengeProgress = async (challengeId: number) => {
         completed: true,
       })
       .where(eq(challengeProgress.id, existingChallengeProgress.id))
+    await db
+      .update(userProgress)
+      .set({
+        hearts: Math.min(currentUserProgress.hearts + 1, 5),
+        points: currentUserProgress.points + 10,
+      })
+      .where(eq(userProgress.userId, userId))
+
+    revalidatePath('/learn')
+    revalidatePath('lesson')
+    revalidatePath('/quests')
+    revalidatePath('/leaderboard')
+    revalidatePath('/lesson/${lessonId}')
+
+    return
   }
+
+  await db.insert(challengeProgress).values({
+    challengeId,
+    userId,
+    completed: true,
+  })
 
   await db
     .update(userProgress)
     .set({
-      hearts: Math.min(currentUserProgress.hearts + 1, 5),
       points: currentUserProgress.points + 10,
     })
     .where(eq(userProgress.userId, userId))
+
+  revalidatePath('/learn')
+  revalidatePath('lesson')
+  revalidatePath('/quests')
+  revalidatePath('/leaderboard')
+  revalidatePath('/lesson/${lessonId}')
 }
