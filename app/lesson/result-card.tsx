937ced78d3fc69b4f799a6cf7fa168372e5f6554
贝吉塔大王 <cyfyifanchen@gmail.com>
2024-04-06 21:36:17 +0800

    Update result-card.tsx

--- app/lesson/result-card.tsx
+++ app/lesson/result-card.tsx
@@ -1,8 +1,28 @@
+import Image from 'next/image'
+import { cn } from '@/lib/utils'
+
 type Props = {
   value: number
   variant: 'points' | 'hearts'
 }
 
-export const ResultCard = () => {
-  return <div className="">Result Card Page.</div>
+export const ResultCard = ({ value, variant }: Props) => {
+  return (
+    <div
+      className={cn(
+        'rounded-2xl border-2 w-full',
+        variant === 'points' && 'bg-orange-400 border-orange-400',
+        variant === 'hearts' && 'bg-rose-500 border-rose-500'
+      )}
+    >
+      <div
+        className={cn(
+          'p-1.5 text-white rounded-t-xl font-bold text-center uppercase text-xs',
+          variant === 'hearts' && 'bg-rose-500',
+          variant === 'points' && 'bg-orange-400'
+        )}
+        {variant === 'haerts' ? 'Hearts Left' : 'Total XP'}
+      ></div>
+    </div>
+  )
 }
