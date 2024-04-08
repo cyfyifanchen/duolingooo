ddccdd8fc55a22262854b26dd9b9ce4ccb9a6d11
贝吉塔大王 <cyfyifanchen@gmail.com>
2024-04-09 07:44:41 +0800

    Update page.tsx

--- app/lesson/page.tsx
+++ app/lesson/page.tsx
@@ -22,7 +22,7 @@ const LessonPage = async () => {
       initialLessonChallenges={lesson.challenges}
       initialHearts={userProgress.hearts}
       initialPercentage={initialPercentage}
-      userSubscription={undefined}
+      userSubscription={null}
     />
   )
 }
