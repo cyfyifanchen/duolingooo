98adeea501754b7b2b891ee1ae8a4309181e38ec
贝吉塔大王 <cyfyifanchen@gmail.com>
2024-04-09 21:19:42 +0800

    Adding items

---
+++ app/(main)/shop/items.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+type Props = {
+  hearts: number
+  points: number
+  hasActiveSubscription: boolean
+}
+
+export const Items = ({ hearts, points, hasActiveSubscription }: Props) => {
+  return <div className="">Item</div>
+}
