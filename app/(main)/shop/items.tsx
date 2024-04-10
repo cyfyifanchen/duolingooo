be691658d8ef17222d80c0894d8b747af10fbb21
贝吉塔大王 <cyfyifanchen@gmail.com>
2024-04-10 21:32:18 +0800

    Update items.tsx

--- app/(main)/shop/items.tsx
+++ app/(main)/shop/items.tsx
@@ -1,8 +1,10 @@
 'use client'
 
+import { refillHearts } from '@/actions/user-progress'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import { useTransition } from 'react'
+import { toast } from 'sonner'
 
 export const POSTS_TO_REFILL = 10
 
@@ -21,6 +23,7 @@ export const Items = ({ hearts, points, hasActiveSubscription }: Props) => {
     }
 
     startTransition(() => {})
+    refillHearts().catch(() => toast.error('Something went wrong'))
   }
 
   return (
