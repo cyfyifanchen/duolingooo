3ada744bc1c20faa95b19eaa48f4e14db29cb691
贝吉塔大王 <cyfyifanchen@gmail.com>
2024-04-15 07:07:07 +0800

    Update schema.ts

--- db/schema.ts
+++ db/schema.ts
@@ -134,3 +134,12 @@ export const userProgressRelations = relations(userProgress, ({ one }) => ({
     references: [courses.id],
   }),
 }))
+
+export const userSubscription = pgTable('user_subscription', {
+  id: serial('id').primaryKey(),
+  userId: text('user_id').notNull().unique(),
+  stripeCustomerId: text('stripe_customer_id').notNull().unique(),
+  stripeSubscriptionId: text('stripe_subscription_id').notNull().unique(),
+  stripePriceId: text('stripe_price_id').notNull(),
+  stripePriceId: text('stripe_price_id').notNull(),
+})
