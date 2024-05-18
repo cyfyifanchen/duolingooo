5573ca725968bcc6ccac17404e014d727d3d7009
贝吉塔大王 <cyfyifanchen@gmail.com>
2024-05-19 07:37:06 +0800

    Update user-subscription.ts

--- actions/user-subscription.ts
+++ actions/user-subscription.ts
@@ -1,8 +1,9 @@
 'use server'
 
-import { absoluteUrl } from '@/lib/utils'
 import { auth, currentUser } from '@clerk/nextjs'
+
 import { stripe } from '@/lib/stripe'
+import { absoluteUrl } from '@/lib/utils'
 import { getUserSubscription } from '@/db/queries'
 
 const returnUrl = absoluteUrl('/shop')
@@ -18,10 +19,11 @@ export const createStripeUrl = async () => {
   const userSubscription = await getUserSubscription()
 
   if (userSubscription && userSubscription.stripeCustomerId) {
-    const stripeSession = await stripe.BillingPortal.SessionsResource.create({
+    const stripeSession = await stripe.billingPortal.sessions.create({
       customer: userSubscription.stripeCustomerId,
       return_url: returnUrl,
     })
+
     return { data: stripeSession.url }
   }
 
@@ -31,14 +33,14 @@ export const createStripeUrl = async () => {
     customer_email: user.emailAddresses[0].emailAddress,
     line_items: [
       {
-        quality: 1,
+        quantity: 1,
         price_data: {
           currency: 'USD',
           product_data: {
-            name: 'Duolingooo Pro',
+            name: 'Lingo Pro',
             description: 'Unlimited Hearts',
           },
-          unit_amount: 2000,
+          unit_amount: 2000, // $20.00 USD
           recurring: {
             interval: 'month',
           },
@@ -51,5 +53,6 @@ export const createStripeUrl = async () => {
     success_url: returnUrl,
     cancel_url: returnUrl,
   })
+
   return { data: stripeSession.url }
 }
