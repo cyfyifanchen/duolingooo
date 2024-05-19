f26eb8ef590a60bf6ded89aacdb9d5c531e2e631
贝吉塔大王 <cyfyifanchen@gmail.com>
2024-05-20 07:09:02 +0800

    Update route.ts

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -1,3 +1,5 @@
+import db from '@/db/drizzle'
+import { userSubscription } from '@/db/schema'
 import { stripe } from '@/lib/stripe'
 import { headers } from 'next/headers'
 import { NextResponse } from 'next/server'
@@ -20,4 +22,26 @@ export async function POST(req: Request) {
       status: 400,
     })
   }
+
+  const session = event.data.object as Stripe.Checkout.Session
+
+  if (event.type === 'checkout.session.completed') {
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    )
+  }
+
+  if (!session?.metadata?.userId) {
+    return new NextResponse('User ID is required', { status: 400 })
+
+    await db.insert(userSubscription).values({
+      userId: session.metadata.userId,
+      stripeSubscriptionId: subscriptIcon.id,
+      stripeCustomerId: SubscriptIcon.customer as string,
+      stripePriceId: SubscriptIcon.items.data[0].price.id,
+      stripeCurrentPeriodEnd: new Date(subscriptIcon.current_period_end * 1000),
+    })
+  }
+
+  return new NextResponse(null, { status: 200 })
 }
