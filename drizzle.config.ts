fab0f97539afb3603594d8266ae4226e4fc9df7b
贝吉塔大王 <cyfyifanchen@gmail.com>
2024-05-20 07:30:51 +0800

    Updating Drizzle kit from 0.20 to 0.21

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,8 +4,10 @@ import type { Config } from 'drizzle-kit'
 export default {
   schema: './db/schema.ts',
   out: './drizzle',
+  dialect: 'postgresql',
   driver: 'pg',
   dbCredentials: {
     connectionString: process.env.DATABASE_URL!,
+    url: '',
   },
 } satisfies Config
