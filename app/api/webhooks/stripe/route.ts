208656c7cb994f170044991153685afe2383bdd5
贝吉塔大王 <cyfyifanchen@gmail.com>
2024-05-20 07:13:51 +0800

    Update route.ts

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -1,9 +1,11 @@
-import db from '@/db/drizzle'
-import { userSubscription } from '@/db/schema'
-import { stripe } from '@/lib/stripe'
+import Stripe from 'stripe'
+import { eq } from 'drizzle-orm'
 import { headers } from 'next/headers'
 import { NextResponse } from 'next/server'
-import Stripe from 'stripe'
+
+import db from '@/db/drizzle'
+import { stripe } from '@/lib/stripe'
+import { userSubscription } from '@/db/schema'
 
 export async function POST(req: Request) {
   const body = await req.text()
@@ -29,18 +31,34 @@ export async function POST(req: Request) {
     const subscription = await stripe.subscriptions.retrieve(
       session.subscription as string
     )
-  }
 
-  if (!session?.metadata?.userId) {
-    return new NextResponse('User ID is required', { status: 400 })
+    if (!session?.metadata?.userId) {
+      return new NextResponse('User ID is required', { status: 400 })
+    }
 
     await db.insert(userSubscription).values({
       userId: session.metadata.userId,
-      stripeSubscriptionId: subscriptIcon.id,
-      stripeCustomerId: SubscriptIcon.customer as string,
-      stripePriceId: SubscriptIcon.items.data[0].price.id,
-      stripeCurrentPeriodEnd: new Date(subscriptIcon.current_period_end * 1000),
+      stripeSubscriptionId: subscription.id,
+      stripeCustomerId: subscription.customer as string,
+      stripePriceId: subscription.items.data[0].price.id,
+      stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000),
     })
+  }
+
+  if (event.type === 'invoice.payment_succeeded') {
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    )
+
+    await db
+      .update(userSubscription)
+      .set({
+        stripePriceId: subscription.items.data[0].price.id,
+        stripeCurrentPeriodEnd: new Date(
+          subscription.current_period_end * 1000
+        ),
+      })
+      .where(eq(userSubscription.stripeSubscriptionId, subscription.id))
   }
 
   return new NextResponse(null, { status: 200 })
